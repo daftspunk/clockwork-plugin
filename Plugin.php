403cc59fb7b48e94060e3905b16369b6af0e9a95
user <user@M.home.my>
2014-12-11 19:52:21 +0200

    Initial plugin version

---
+++ Plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Qub\Clockwork;
+
+use System\Classes\PluginBase;
+use Illuminate\Support\Facades\App;
+use Illuminate\Foundation\AliasLoader;
+
+class Plugin extends PluginBase
+{
+    use \System\Traits\ConfigMaker;
+
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Qub.Clockwork',
+            'description' => 'Clockwork',
+            'author'      => 'Andriy Kuzyk',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    public function register()
+    {
+//        App::register( 'Clockwork\Support\Laravel\ClockworkServiceProvider' );
+//
+//        $alias = AliasLoader::getInstance();
+//        $alias->alias( 'Clockwork', 'Clockwork\Support\Laravel\Facade' );
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Clockwork integration plugin
+
+This plugin adds Clockwork Developer Tools integration plugin for [OctoberCMS](http://octobercms.com).
